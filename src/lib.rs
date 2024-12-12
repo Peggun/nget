a1b6b5677dde0f8f2896842273d87c9795d2e427
Peggun <125943831+Peggun@users.noreply.github.com>
2024-12-12 14:42:31 +0800

    Licenses for each file fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// This file is part of nget
+//
+// Copyright (C) 2024 Peggun
+//
+// nget is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// nget is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
 pub mod utils;
 
 pub use utils::cli;
--- tests/http_tests.rs
+++ tests/http_tests.rs
@@ -1,3 +1,18 @@
+// This file is part of nget
+//
+// Copyright (C) 2024 Peggun
+//
+// nget is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// nget is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// Run using cargo test
 #[cfg(test)]
 mod tests {
     use nget::error::NgetError;
@@ -8,7 +23,7 @@ mod tests {
         let url = "http://nonexistent.url/file.txt";
         let file_path = "./test_file.txt";
 
-        let result = http::download_file(url, file_path).await; // ik theres errors with the additions to the progress bars. 
+        let result = http::download_file(url, file_path).await; // There are errors due to the addition of the progress bars 
 
         // Assert that the result is an error
         assert!(result.is_err());
